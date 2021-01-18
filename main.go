0c87c00c7ef52c04ef504a4e64a7f8601910b887
tonymj76 <demeyortj@gmail.com>
2021-01-18 21:45:21 +0100

    add help text

--- main.go
+++ main.go
@@ -23,7 +23,25 @@ func checkExtended(str string) bool {
 
 func main() {
 	extended := flag.Bool("extended", false, "check MX record")
+	help := flag.Bool("help", false, "help on commands")
 	flag.Parse()
+	if *help {
+		fmt.Println(`
+		timothy-stephen v1 of January 2021 by Timothy Stephen Mayor
+		usage: timothy-stephen <file input must be CSV> [flag]
+		
+		flags:
+			--help
+			--extended
+
+		example:
+
+		timothy-stephen --help display help information
+		timothy-stephen <file input must be CSV> this prints a json formatter at the root of the directory
+		timothy-stephtn <file input> --extended 
+		`)
+		return
+	}
 	if checkExtended(flag.Arg(1)) {
 		*extended = true
 	}
